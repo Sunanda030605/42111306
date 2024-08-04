ece15b1f9c41dc6d263defdee3f0610b5f685d95
Sunanda N <164130059+Sunanda030605@users.noreply.github.com>
2024-08-04 19:04:23 +0530

    Add files via upload

---
+++ 2024-07-26/List.py
@@ -0,0 +1,10 @@
+
+L = [8,2,4,1]
+
+# Extend the list with [5, 6, 7]
+L.extend([5, 6, 7])
+print(L) 
+
+# Remove the 5th value (index 4)
+L.pop(4)
+print(L)  
---
+++ 2024-07-26/String.py
@@ -0,0 +1,18 @@
+
+name = "sunanda"
+
+# Convert to upper case
+upper_name = name.upper()
+print(upper_name)
+
+# Convert to lower case
+lower_name = name.lower()
+print(lower_name)  
+
+# Capitalize the first letter
+capitalized_name = name.capitalize()
+print(capitalized_name)  
+
+# Replace 's' with 'S'
+replaced_name = name.replace('s', 'S')
+print(replaced_name)
